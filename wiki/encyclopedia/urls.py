0ae9098ccc49df79be901be150017aac68b38a01
perugi <dominik.perusko@gmail.com>
2022-12-09 15:09:08 +0100

    Add search functionality

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.render_page, name="render_page"),
+    path("search/", views.search, name="search"),
+    path("add/", views.add_page, name="add_page"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,8 +1,14 @@
+from django import forms
 from django.shortcuts import render
 from django.http import HttpResponse
 import markdown2
 
 from . import util
+
+
+class EditPageForm(forms.Form):
+    title = forms.CharField(label="Title", max_length=50, name="title", required=True)
+    content = forms.CharField(label="Content", widget=forms.Textarea, name="content")
 
 
 def index(request):
@@ -19,3 +25,17 @@ def render_page(request, title):
         )
     else:
         return render(request, "encyclopedia/error.html", {"title": title})
+
+
+def search(request):
+    if request.method == "POST":
+        pages = util.list_entries()
+        search = request.POST["q"].lower()
+        results = [page for page in pages if search in page.lower()]
+
+    return render(request, "encyclopedia/search.html", {"results": results})
+
+
+def add_page(request):
+
+    return render(request, "encyclopedia/add_page.html", {"form": EditPageForm()})
