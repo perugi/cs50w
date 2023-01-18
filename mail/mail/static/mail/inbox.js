9f1cab967e5be3493fccfdb952c5653b05470fb5
perugi <dominik.perusko@gmail.com>
2023-01-18 23:31:00 +0100

    Add reply and archive, specification complete

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -4,23 +4,35 @@ document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
-  document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose').addEventListener('click', () => compose_email());
 
   // By default, load the inbox
   load_mailbox('inbox');
 });
 
-function compose_email() {
+function compose_email(replied_mail) {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#read-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
-  // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
+  // This is a new mail, clear out the compose fields.
+  if (replied_mail === undefined) {
+    document.querySelector('#compose-recipients').value = '';
+    document.querySelector('#compose-subject').value = '';
+    document.querySelector('#compose-body').value = '';
+  } else {
+    // We should not add a new "Re: ", if the subject already contains it.
+    if (!(replied_mail.subject.startsWith("Re: "))) {
+      replied_mail.subject = `Re: ${replied_mail.subject}`;
+    }
+
+    document.querySelector('#compose-recipients').value = replied_mail.sender;
+    document.querySelector('#compose-subject').value = replied_mail.subject;
+    document.querySelector('#compose-body').value =
+      `\n\nOn ${replied_mail.timestamp} ${replied_mail.sender} wrote:\n${replied_mail.body}`;
+  }
 
   document.querySelector('#compose-form').onsubmit = () => {
     recipients = document.querySelector('#compose-recipients').value;
@@ -45,6 +57,7 @@ function compose_email() {
     return false;
   }
 }
+
 
 function load_mailbox(mailbox) {
 
@@ -110,16 +123,58 @@ function read_mail(email) {
 
   // Populate the mail info
   const sender = document.createElement('div');
-  sender.innerHTML = `<b>From:</b> ${email.sender}`;
+  sender.innerHTML = `<b>From:</b> ${email.sender} `;
   const recipients = document.createElement('div');
-  recipients.innerHTML = `<b>To:</b> ${email.recipients}`;
+  recipients.innerHTML = `<b>To:</b> ${email.recipients} `;
   const subject = document.createElement('div');
-  subject.innerHTML = `<b>Subject:</b> ${email.subject}`;
+  subject.innerHTML = `<b>Subject:</b> ${email.subject} `;
   const timestamp = document.createElement('div');
-  timestamp.innerHTML = `<b>Timestamp:</b> ${email.timestamp}`;
+  timestamp.innerHTML = `<b>Timestamp:</b> ${email.timestamp} `;
 
-  document.querySelector('#read-info').append(sender, recipients, subject, timestamp);
+  const reply_button = document.createElement('button');
+  reply_button.innerHTML = "Reply";
+  reply_button.className = "btn btn-sm btn-outline-primary";
+  reply_button.id = "reply-button";
+  reply_button.addEventListener('click', () => {
+    compose_email(email);
+  });
+
+  document.querySelector('#read-info').append(sender, recipients, subject, timestamp,
+    reply_button);
+
+  console.log(email.sender, document.querySelector('#user-id').innerHTML);
+  if (email.sender !== document.querySelector('#user-id').innerHTML) {
+    const archive_button = document.createElement('button');
+    if (email.archived === false) {
+      archive_button.innerHTML = "Archive";
+    } else {
+      archive_button.innerHTML = "Unarchive";
+
+    }
+    archive_button.className = "btn btn-sm btn-outline-primary";
+    archive_button.id = "archive-button";
+    archive_button.addEventListener('click', () => {
+      archive_mail(email);
+    });
+    document.querySelector('#read-info').append(archive_button)
+  }
+
 
   // Populate the mail body
   document.querySelector('#read-body').innerHTML = email.body
+  console.log(email.body)
+}
+
+function archive_mail(email) {
+
+  // Change the state of the archived value of the mail.
+  fetch(`emails/${email.id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      archived: !(email.archived)
+    })
+  })
+    .then(() => {
+      load_mailbox('inbox');
+    })
 }
